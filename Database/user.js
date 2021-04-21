f804fcaa871dd8675f06038ee791debdcc5fb9bd
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-04-21 16:21:03 +0000

    created a user database

--- Database/user.js
+++ Database/user.js
@@ -1,1 +1,10 @@
-const {} = require
+const {Schema, model} = require('mongoose')
+
+const userSchema = new Schema({
+    username: String, 
+    password: String, 
+    email: String, 
+    createdAt: String
+})
+
+module.export = model('user', userSchema)
