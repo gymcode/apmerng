dc1b8f8b764dc0594ca151fdcf8f09172f181b2e
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-04-21 19:38:40 +0000

    ride

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,12 +1,12 @@
 const postResolver = require('./postResolver')
-const userMutation = require('.')
+const userMutation = require('./userResolver')
 
 module.exports = {
     Query: {
         ...postResolver.Query
     }, 
     Mutation: {
-        ...
+        ...userMutation.Mutation
     }
 
 }
--- graphql/resolvers/userResolver.js
+++ graphql/resolvers/userResolver.js
@@ -2,6 +2,8 @@ const User = require('../../Database/user')
 
 module.exports = {
     Mutation: {
-        
+        register: (_, {registerUser: {username, email, password, confirmPassword}})=>{
+            
+        }
     }
 }
