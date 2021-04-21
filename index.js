e6f23700cb7391f5c095b5936723b869dc5cd7eb
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-04-21 13:33:42 +0000

    apollo stuff

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-const ApolloServer = require('ap')
+const {ApolloServer} = require('apollo-server')
+const gql = require('graphql-tag')
+
+
+const typeDefs = gql `
+    type Query {
+        sayHello: String!
+    }
+`
+
+const resolvers = {
+    Query: {
+        sayHello: () => "Hello world"
+    }
+}
+
+// creating our server
+const server = new ApolloServer({
+    typeDefs: typeDefs, 
+    resolvers: resolvers
+})
+
+server.listen({port:  5000})
+    .then((res)=>{
+        console.log(`server running on ${res.url}`)
+    })
