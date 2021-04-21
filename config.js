4b8401a2d09024fa2b75a21ff60c3e49323491d1
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-04-21 19:46:55 +0000

    on it

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    MONGODB: 'mongodb+srv://Kenneth:KsMjuhJs9gOaVEpX@gql.0uwcz.mongodb.net/Trial?retryWrites=true&w=majority' 
+    MONGODB: 'mongodb+srv://Kenneth:KsMjuhJs9gOaVEpX@gql.0uwcz.mongodb.net/Trial?retryWrites=true&w=majority', 
+    SALT: 12, 
+    SECRET: "i am the best"
 }
--- graphql/resolvers/userResolver.js
+++ graphql/resolvers/userResolver.js
@@ -1,11 +1,16 @@
 const User = require('../../Database/user')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const { } = require('../../config')
 
 module.exports = {
     Mutation: {
-        register: (_, {registerUser: {username, email, password, confirmPassword}})=>{
+        register: async (_, {registerUser: {username, email, password, confirmPassword}})=>{
             // user validation 
             // checking if user already exists
             // hashing the user's password 
+            password = await bcrypt.hash(password, 12)
         }
     }
 }
