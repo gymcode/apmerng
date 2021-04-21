79327f5b16c03a9c52025d69597e9c6671756c36
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-04-21 19:43:02 +0000

    user stuff

--- graphql/resolvers/userResolver.js
+++ graphql/resolvers/userResolver.js
@@ -3,7 +3,9 @@ const User = require('../../Database/user')
 module.exports = {
     Mutation: {
         register: (_, {registerUser: {username, email, password, confirmPassword}})=>{
-            
+            // user validation 
+            // checking if user already exists
+            // hashing the user's password 
         }
     }
 }
