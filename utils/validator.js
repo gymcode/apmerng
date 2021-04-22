049e1ffecc779e0a05d54b4db735e3812ebc6cc6
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-04-22 10:38:48 +0000

    we lift our hands

--- utils/validator.js
+++ utils/validator.js
@@ -1,3 +1,13 @@
-module.exports.ValidateRegisterInput = (email, )=>{
+module.exports.ValidateRegisterInput = (email, username, password, confirmpassword)=>{
+    const errors = {}
 
+    if (username.trim() == "" ) {
+        errors.username = "username field must not be left empty"
+    }
+
+    if (email.trim() == "") {
+        errors.email = "email field must not be left empty"
+    } else {
+        const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    }
 }
