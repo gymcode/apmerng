a7f6014f9779606976cd40f6393f82b66fbbdb3a
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-04-22 10:48:35 +0000

    validator

--- utils/validator.js
+++ utils/validator.js
@@ -9,5 +9,19 @@ module.exports.ValidateRegisterInput = (email, username, password, confirmpasswo
         errors.email = "email field must not be left empty"
     } else {
         const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        if (!email.match(regex)) {
+            errors.email = "email does not match the correct format of an email"
+        }
+    }
+
+    if (password === "") {
+        errors.password = "password file must not be left empty"
+    } else if (password !== confirmpassword) {
+        errors.confirmpassword = "passwords do not match"
+    }
+
+    return {
+        errors, 
+        valid: Object.keys(errors) > 1
     }
 }
