92cf17f3d4fd75b39bb48d7013a5e7334c19a0cd
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-04-23 14:32:04 +0000

    roll

--- utils/check-auth.js
+++ utils/check-auth.js
@@ -3,5 +3,22 @@ const {AuthenticationError} = require('apollo-server')
 const { SECRET } = require('../config')
 
 module.exports = (context)=>{
+    const authheader = context.req.headers.authorization; 
 
+    if (authheaders) {
+        const Token = authheader.split('Bearer ')[1]
+        console.log(Token)
+        if (Token) {
+            try {
+                const user = jwt.verify(Token, SECRET)
+                return user
+            } catch (error) {
+                throw new AuthenticationError("invalid/expired token")
+            }
+        } else {
+            throw new AuthenticationError("Authentication token must be \' Bearer ")
+        }
+    } else {
+        throw new AuthenticationError("Authentication toke must be provided ")
+    }
 }
